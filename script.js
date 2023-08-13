c8736bbd704d2791ca44501b3a5efe79f2433959
StetrickSP <stetricksp@gmail.com>
2023-08-13 12:04:06 +0700

    commit

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+// let x = 0;
+// while (x < 10)
+// {
+//     console.log(x += 1);
+// }
 let x = 0;
-while (x < 10)
+
+// do 
+// {
+//     console.log(x++)
+// }
+// while (x < 10)
+// {}
+
+
+// for (var i = 0; i <= 10; i++)
+// {
+//     alert(i);
+// }
+
+let myNumbers = [1, 2, 3, 4, 5];
+let myStrings = ["Huy", "Tai", "An", "Minh"];
+
+// for (var i = 0; i < 3; i++)
+// {
+//     alert(i+":"+myStrings[i]);
+// }
+do
 {
-    console.log(x += 1);
+    alert(x+":"+myStrings[x])
+    x += 1;
 }
+while (x <= 3)
+
